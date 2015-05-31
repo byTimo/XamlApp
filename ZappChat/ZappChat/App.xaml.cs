f590e4c961ed6e069c60d0061b2bf7e894456283
byTimo <Race.timo@gmail.com>
2015-05-31 16:35:25 +0600

    - лончер пока + Update вшит в клиент

--- ZappChat/ZappChat/App.xaml.cs
+++ ZappChat/ZappChat/App.xaml.cs
@@ -5,12 +5,9 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 using Hardcodet.Wpf.TaskbarNotification;
-using NAppUpdate.Framework;
-using NAppUpdate.Framework.Sources;
 using ZappChat.Core;
 using ZappChat.Core.Socket;
 
@@ -29,7 +26,7 @@ namespace ZappChat
         public const double IntervalBetweenReshowNotificationInSecond = 300.0;
 
         public const string WebSocketUrl = "ws://zappchat.ru:7778";
-        public const string UpdateFeedUrl = "http://zappchat.ru/setup/updates";
+        public const string UpdateFeedUrl = "http://zappchat.spyric.ru/ReleaseTest/feed.xml";
 
         public static ConnectionStatus ConnectionStatus { get; set; }
         public static ulong LastLogId { get; set; }
@@ -53,38 +50,29 @@ namespace ZappChat
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            var comLineArgs = Environment.GetCommandLineArgs();
-            if (comLineArgs.Length < 2)
-            {
-                MessageBox.Show("Запускайте программу через ZappChatLauncher.exe");
-                Current.Shutdown();
-                return;
-            }
-            var test = comLineArgs.Aggregate("", (current, comLineArg) => current + comLineArg);
-            var isLastVersion = false;
-            try
-            {
-                isLastVersion = bool.Parse(comLineArgs[1]);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Ошибка в обновлении");
-                Current.Shutdown();
-                return;
-            }
-            if (!isLastVersion)
-            {
-                MessageBox.Show("Ошибка в обновлении");
-                Current.Shutdown();
-                return;
-            }
-
-            MessageBox.Show("Эта штука не должна появиться при запуске!");
             LastLogId = 0;
             FileDispetcher.InitializeFileDispetcher();
             DialoguesStatuses = FileDispetcher.ReadAllCollection(FileDispetcher.FullPathToDialogueInformation);
             login = new LoginWindow();
             main = new MainWindow();
+
+            AppUpdateManager.SetUrlRemoteServer(UpdateFeedUrl);
+
+            var socketOpener = new BackgroundWorker();
+            socketOpener.DoWork += (o, args) => AppWebSocketEventManager.OpenWebSocket();
+            login.Show();
+            currentWindow = OpenedWindow.Login;
+            AppUpdateManager.StartupCheckAndPrepareUpdateFeeds(b =>
+            {
+                if (b)
+                {
+                    socketOpener.RunWorkerAsync();
+                    return;
+                }
+                MessageBox.Show("Ошибка в обновлении!");
+                Current.Dispatcher.Invoke(Current.Shutdown);
+            });
+
 
             AppEventManager.Connect += o =>
             {
@@ -113,13 +101,6 @@ namespace ZappChat
             updateCountersDispatcherTimer = new DispatcherTimer {Interval = TimeSpan.FromMinutes(UpdateControlTimeIntervalInMinutes)};
             updateCountersDispatcherTimer.Tick += (o, args) => AppEventManager.UpdateCounterEvent();
             updateCountersDispatcherTimer.Start();
-
-            var socketOpener = new BackgroundWorker();
-            socketOpener.DoWork += (o, args) => AppWebSocketEventManager.OpenWebSocket();
-            login.Show();
-            currentWindow = OpenedWindow.Login;
-            socketOpener.RunWorkerAsync();
-            
         }
 
         private void AuthorizationSuccess(object sender, AuthorizationType type)
---
+++ ZappChat/ZappChat/Core/AppUpdateManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using NAppUpdate.Framework;
+using NAppUpdate.Framework.Common;
+using NAppUpdate.Framework.Sources;
+
+namespace ZappChat.Core
+{
+    internal static class AppUpdateManager
+    {
+        public static void SetUrlRemoteServer(string url)
+        {
+            UpdateManager.Instance.UpdateSource = new SimpleWebSource(url);
+            UpdateManager.Instance.ReinstateIfRestarted();
+        }
+
+        public static void StartupCheckAndPrepareUpdateFeeds(Action<bool> callBack)
+        {
+            var updManager = UpdateManager.Instance;
+            updManager.CheckForUpdates();
+            if (updManager.UpdatesAvailable == 0)
+            {
+                callBack.Invoke(true);
+                return;
+            }
+            updManager.BeginPrepareUpdates(ar => OnPrepareUpdatesCompleted(ar, callBack), null);
+        }
+
+
+
+        private static void OnPrepareUpdatesCompleted(IAsyncResult prepareResult, Action<bool> callBack)
+        {
+            try
+            {
+                ((UpdateProcessAsyncResult) prepareResult).EndInvoke();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Updates preperation failed. Check the feed and try again.{0}{1}",
+                    Environment.NewLine, ex));
+                callBack.Invoke(false);
+                return;
+            }
+            try
+            {
+                UpdateManager.Instance.ApplyUpdates(true);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Error while trying to install software updates{0}{1}",
+                    Environment.NewLine, ex));
+                callBack.Invoke(false);
+            }
+            UpdateManager.Instance.CleanUp();
+        }
+
+    }
+}
